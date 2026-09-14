de07a2c4151a0080fc21cdab0a6c1ab16871cbf6
Charles <adeakinyemi129@gmail.com>
2026-09-14 23:13:43 +0100

    fixed payment callback url

--- index.js
+++ index.js
@@ -235,10 +235,10 @@ connectDb()
 const allowedOrigins = [
   "https://www.quickinvoiceng.com",
   "https://quickinvoiceng.com",
-  // "http://localhost:3000",
-  // "https://quick-invoice-frontend-two.vercel.app",
-  // "https://test-quickinvoice-frontend.vercel.app",
-  // "https://www.test-quickinvoice-frontend.vercel.app"
+  "http://localhost:3000",
+  "https://quick-invoice-frontend-two.vercel.app",
+  "https://test-quickinvoice-frontend.vercel.app",
+  "https://www.test-quickinvoice-frontend.vercel.app"
 ];
 
 app.use(cors({
