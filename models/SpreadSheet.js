7f699db4c07cd4e2f4c74567a8b66301dedf8bbf
Charles <adeakinyemi129@gmail.com>
2026-09-16 22:04:06 +0100

    updated model

--- models/SpreadSheet.js
+++ models/SpreadSheet.js
@@ -5,7 +5,7 @@ const SpreadsheetRowSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.Mixed, required: true }, // Mixed allows string or number (Date.now())
   date: { type: String, default: '' },
   units: { type: mongoose.Schema.Types.Mixed, default: '' },
-  productName: { type: String, default: '' },
+  collector: { type: String, default: '' },
   description: { type: String, default: '' },
   sn: { type: String, default: '' },
   imei: { type: String, default: '' },
